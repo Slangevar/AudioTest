4ad54fb370e06b770dc5b913f3d1e20a0ed0549b
Slangevar <37900579+Slangevar@users.noreply.github.com>
2020-02-21 21:06:52 -0500

    Update xf_demo.py

--- xf_demo.py
+++ xf_demo.py
@@ -1,6 +1,6 @@
 # -*- coding:utf-8 -*-
 #
-#   author: iflytek
+#   author: iflytek, Slangevar
 #
 #  本demo测试时运行的环境为：Windows + Python3.7
 #  本demo测试成功运行时所安装的第三方库及其版本如下，您可自行逐一或者复制到一个新的txt文件利用pip一次性安装：
@@ -37,22 +37,22 @@ import _thread as thread
 import socket
 import re
 
-STATUS_FIRST_FRAME = 0  # 第一帧的标识
-STATUS_CONTINUE_FRAME = 1  # 中间帧标识
-STATUS_LAST_FRAME = 2  # 最后一帧的标识
+STATUS_FIRST_FRAME = 0
+STATUS_CONTINUE_FRAME = 1
+STATUS_LAST_FRAME = 2
 NAME = "output.txt"
 
 class Ws_Param(object):
-    # 初始化
+
     def __init__(self, APPID, APIKey, APISecret, AudioFile):
         self.APPID = APPID
         self.APIKey = APIKey
         self.APISecret = APISecret
         self.AudioFile = AudioFile
 
-        # 公共参数(common)
+
         self.CommonArgs = {"app_id": self.APPID}
-        # 业务参数(business)，更多个性化参数可在官网查看
+
         self.BusinessArgs = {"domain": "iat", "language": "zh_cn", "accent": "mandarin", "vinfo":1,"vad_eos":10000}
 
     # 生成url
@@ -62,11 +62,11 @@ class Ws_Param(object):
         now = datetime.now()
         date = format_date_time(mktime(now.timetuple()))
 
-        # 拼接字符串
+
         signature_origin = "host: " + "ws-api.xfyun.cn" + "\n"
         signature_origin += "date: " + date + "\n"
         signature_origin += "GET " + "/v2/iat " + "HTTP/1.1"
-        # 进行hmac-sha256进行加密
+
         signature_sha = hmac.new(self.APISecret.encode('utf-8'), signature_origin.encode('utf-8'),
                                  digestmod=hashlib.sha256).digest()
         signature_sha = base64.b64encode(signature_sha).decode(encoding='utf-8')
@@ -74,25 +74,20 @@ class Ws_Param(object):
         authorization_origin = "api_key=\"%s\", algorithm=\"%s\", headers=\"%s\", signature=\"%s\"" % (
             self.APIKey, "hmac-sha256", "host date request-line", signature_sha)
         authorization = base64.b64encode(authorization_origin.encode('utf-8')).decode(encoding='utf-8')
-        # 将请求的鉴权参数组合为字典
+
         v = {
             "authorization": authorization,
             "date": date,
             "host": "ws-api.xfyun.cn"
         }
-        # 拼接鉴权参数，生成url
+
         url = url + '?' + urlencode(v)
-        # print("date: ",date)
-        # print("v: ",v)
-        # 此处打印出建立连接时候的url,参考本demo的时候可取消上方打印的注释，比对相同参数时生成的url与自己代码生成的url是否一致
-        # print('websocket url :', url)
         return url
 
 wsParam = Ws_Param(APPID='5e4e1c0b', APIKey='a4409a608ac6b8fb39d46ed6a7a430bd',
                     APISecret='6e4f1fd8e85acfc969bef1bf82341e9a',
                     AudioFile=r'E:\Learning\Python\NLP related\iat_ws_python3_demo\output.pcm')
 
-# 收到websocket消息的处理
 def on_message(ws, message):
     try:
         code = json.loads(message)["code"]
@@ -102,38 +97,30 @@ def on_message(ws, message):
             print("sid:%s call error:%s code is:%s" % (sid, errMsg, code))
 
         else:
-            # print(message)
             data = json.loads(message)["data"]["result"]["ws"]
-            # print(json.loads(message))
             result = ""
             for i in data:
                 for w in i["cw"]:
-                    result += w["w"] # This is the final sentence
+                    result += w["w"]
             with open(file=NAME, mode='a') as f:
-                f.write(result) # write the result into a txt file
-            # print("sid:%s call success!,data is:%s" % (sid, json.dumps(data, ensure_ascii=False)))
+                f.write(result)
+
     except Exception as e:
         print("receive msg,but parse exception:", e)
 
-# 收到websocket错误的处理
 def on_error(ws, error):
     print("### error:", error)
 
-
-# 收到websocket关闭的处理
 def on_close(ws):
     with open("output.txt", "r") as f:
         text = f.read()
         print("Result: " + text)
-    #print("### closed ###")
 
-
-# 收到websocket连接建立的处理
 def on_open(ws):
     def run(*args):
-        frameSize = 8000  # 每一帧的音频大小
-        intervel = 0.04  # 发送音频间隔(单位:s)
-        status = STATUS_FIRST_FRAME  # 音频的状态信息，标识音频是第一帧，还是中间帧、最后一帧
+        frameSize = 8000
+        intervel = 0.04
+        status = STATUS_FIRST_FRAME
 
         with open(wsParam.AudioFile, "rb") as fp:
             while True:
@@ -141,9 +128,7 @@ def on_open(ws):
                 # 文件结束
                 if not buf:
                     status = STATUS_LAST_FRAME
-                # 第一帧处理
-                # 发送第一帧音频，带business 参数
-                # appid 必须带上，只需第一帧发送
+
                 if status == STATUS_FIRST_FRAME:
 
                     d = {"common": wsParam.CommonArgs,
@@ -154,13 +139,13 @@ def on_open(ws):
                     d = json.dumps(d)
                     ws.send(d)
                     status = STATUS_CONTINUE_FRAME
-                # 中间帧处理
+
                 elif status == STATUS_CONTINUE_FRAME:
                     d = {"data": {"status": 1, "format": "audio/L16;rate=16000",
                                   "audio": str(base64.b64encode(buf), 'utf-8'),
                                   "encoding": "raw"}}
                     ws.send(json.dumps(d))
-                # 最后一帧处理
+
                 elif status == STATUS_LAST_FRAME:
                     d = {"data": {"status": 2, "format": "audio/L16;rate=16000",
                                   "audio": str(base64.b64encode(buf), 'utf-8'),
@@ -168,16 +153,8 @@ def on_open(ws):
                     ws.send(json.dumps(d))
                     time.sleep(1)
                     break
-                # 模拟音频采样间隔
+
                 time.sleep(intervel)
         ws.close()
 
     thread.start_new_thread(run, ())
-
-
-
-# 测试时候在此处正确填写相关信息即可运行
-# if __name__ == "__main__":
-
-    # time2 = datetime.now()
-    # print(time2-time1)
